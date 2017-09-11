1f392cfac533148258b3d07f0e5e18b6e57ab09c
carlosgrn94 <31866173+carlosgrn94@users.noreply.github.com>
2017-09-11 18:12:18 -0400

    Add files via upload

---
+++ Lab01/src/Lab01.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class Lab01 {
+	
+	public static void main(String args[]) throws IOException{
+		
+		Scanner input = null;
+		
+		int t = 0;
+		
+		System.out.println();
+		
+		//open file
+		try{
+			input = new Scanner(Paths.get("."+File.separator+"bin"+File.separator+"Events.txt"));
+		} catch(IOException e){
+			System.err.println("Io " + e);
+		}
+		
+		//read file
+		try{
+			while(input.hasNext()){
+				System.out.printf("Type = %-12d Month: %-12d Day: %-12d Year: %-12d "
+						+ "Hour: %-12d Minute: %-12d Description: %-20s Extra: %-12s \n", 
+						t = input.nextInt(), input.nextInt(), input.nextInt(), input.nextInt(), 
+						input.nextInt(), input.nextInt(), input.next(), (t==1 || t==3) ? input.next() : "");
+			}
+		} catch(NoSuchElementException e){
+			System.err.println("Such " + e);
+		} catch(IllegalStateException e){
+			System.err.println("Ill " + e);
+		}
+		
+		//close file
+		if(input != null)
+			input.close();
+		
+	}
+
+}
